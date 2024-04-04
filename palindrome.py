3253ebcb2c9a2f48c1f09a2c93828d07680e03c7
Ale-Sanchez2 <as.sanchez@alumno.um.edu.ar>
2024-04-04 20:38:03 -0300

    Agrego mas tests.

--- palindrome.py
+++ palindrome.py
@@ -5,7 +5,7 @@ def is_palindrome(palindrome_con_espacio):
     palindrome_sin_espacio= ""
     for i in range(0, len(palindrome_con_espacio)):
         if palindrome_con_espacio[i] != " ":
-            palindrome_sin_espacio += palindrome_con_espacio[i]
+            palindrome_sin_espacio += palindrome_con_espacio[i].lower()
     
     for indice in range(0, len(palindrome_sin_espacio)):
         
@@ -15,24 +15,61 @@ def is_palindrome(palindrome_con_espacio):
     return True
             
         
-
-
 class TestPalindrome (unittest.TestCase):
     def test_A (self):
         resultado = is_palindrome('a')
         self.assertEqual(resultado, True)
-    def test_C (self):
+    def test_aa (self):
         resultado = is_palindrome('aa')
         self.assertEqual(resultado, True)
-    def test_B (self):
+    def test_ab (self):
+        resultado = is_palindrome('ab')
+        self.assertEqual(resultado, False)
+    def test_aba (self):
+        resultado = is_palindrome('aba')
+        self.assertEqual(resultado, True)
+    def test_acb (self):
+        resultado = is_palindrome('acb')
+        self.assertEqual(resultado, False)
+    def test_abba (self):
+        resultado = is_palindrome('abba')
+        self.assertEqual(resultado, True)
+    def test_abca (self):
+        resultado = is_palindrome('abca')
+        self.assertEqual(resultado, False)
+    def test_abbc (self):
+        resultado = is_palindrome('abbc')
+        self.assertEqual(resultado, False)
+    def test_acbbca (self):
+        resultado = is_palindrome('acbbca')
+        self.assertEqual(resultado, True)
+    def test_acbsc (self):
+        resultado = is_palindrome('acbsc')
+        self.assertEqual(resultado, False)
+    def test_neuquen (self):
         resultado = is_palindrome('neuquen')
         self.assertEqual(resultado, True)
-    def test_Q (self):
+    def test_neuqen (self):
+        resultado = is_palindrome('neuqen')
+        self.assertEqual(resultado, False)
+    def test_Neunem (self):
+        resultado = is_palindrome('Neunem')
+        self.assertEqual(resultado, False)
+    def test_ELPEP (self):
         resultado = is_palindrome('ELPEP')
         self.assertEqual(resultado, False)
-    def test_X (self):
+    def test_ac_a (self):
         resultado = is_palindrome('ac a')
         self.assertEqual(resultado, True )
+    def test_Neu_quen (self):
+        resultado = is_palindrome('Neu quen')
+        self.assertEqual(resultado, True)
+    def test_NE_en (self):
+        resultado = is_palindrome('NE en')
+        self.assertEqual(resultado, True)
 
-unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
+
 
